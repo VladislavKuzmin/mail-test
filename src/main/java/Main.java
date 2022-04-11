4555c3391318b1c7ae4743104051607bfa1a7238
VladislavKuzmin <vladislav11.kuzmin1@gmail.com>
2022-04-11 22:18:27 +0300

    almost done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,14 +11,15 @@ import java.time.Duration;
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        ;
         String userName = "test123456789te5t";
         String password = "AOaTUo)iea12";
+
         String url = "https://account.mail.ru/login/?mode=simple&v=2.8.2&account_host=account.mail.ru&type=login&allow_external=1&app_id_mytracker=58519&success_redirect=https%3A%2F%2Fe.mail.ru%2Fmessages%2Finbox%3Fback%3D1&project=home&from=navi&parent_url=https%3A%2F%2Fmail.ru%2F";
 
-        WebDriver driver = new ChromeDriver();
-
         driver.get(url);
-        driver.manage().window().maximize();
+
         WebElement loginField = new WebDriverWait(driver, Duration.ofSeconds(10)).
                 until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
         loginField.sendKeys(userName);
@@ -29,16 +30,23 @@ public class Main {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));  //???
         passwordField.sendKeys(password);
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div/div[2]/div/div/form/div[2]/div/div[3]/div/div/div[1]/div/button")).click();
-        WebElement sendLetterField;
 
-        sendLetterField = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app-canvas\"]/div/div[1]/div[1]/div/div[2]/span/div[1]/div[1]/div/div/div/div[1]/div/div/a")));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        sendLetterField.click();
+        WebElement sendLetterField;
+        try {
+            sendLetterField = new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app-canvas\"]/div/div[1]/div[1]/div/div[2]/span/div[1]/div[1]/div/div/div/div[1]/div/div/a"))); //fulscreen
+            sendLetterField.click();
+        }
+        catch (Exception e){
+            sendLetterField = new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app-canvas\"]/div/div[1]/div[1]/div/div[2]/span/div[1]/div[1]/div/div/div/div[1]/div/div/a")));
+            sendLetterField.click();
+        }
 
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]/div[1]")).sendKeys("Заяц вор");
 
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[1]/span[2]/span/span")).click();
         driver.quit();
+        System.out.println("test completed");
     }
 }
