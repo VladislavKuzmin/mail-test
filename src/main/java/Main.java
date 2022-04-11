e35f9735e08ffb873721103602da4b29a54bb46f
VladislavKuzmin <vladislav11.kuzmin1@gmail.com>
2022-04-11 04:59:40 +0300

    added exception handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
-        String userName = "failmego@mail.ru";
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        String userName = "vladislav11.kuzmin1";
         String password = "2568931kuzmin";
 
         WebDriver driver = new ChromeDriver();
 
-            driver.get("https://mail.ru/");
-            driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[1]/div[2]/div[1]/button")).click();
-                driver.findElement(By.className("input-0-2-71")).click();
-                System.out.println("sasasa");
-            driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[2]/div/div/form/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div/input")).sendKeys(userName + Keys.ENTER);
+        driver.get("https://mail.ru/");
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[1]/div[2]/div[1]/button")).click();
+        try {
+            driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div/div[2]/div/div/form/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div/input")).sendKeys(userName + Keys.ENTER);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
-
 }
