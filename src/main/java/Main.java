5458b9f1e7203109e2ae9e012713a97a99cbc5b1
VladislavKuzmin <vladislav11.kuzmin1@gmail.com>
2022-04-13 10:45:48 +0300

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,14 +19,6 @@ public class Main {
                 .pressSubmitButton()
                 .pressSendLetterButton()
                 .pressSaveButton();
-
-//        loginPage.pressLoginButton();
-//        loginPage.switchToLoginFrame();
-//        loginPage.typeUsername(username);
-//        loginPage.typePassword(password);
-//        loginPage.pressSubmitButton()
-//                .pressSendLetterButton()
-//                .pressSaveButton();
 
         driver.quit();
         System.out.println("test passed");
